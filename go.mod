b16472e6c7a83af5cd4335c840cfdbb3f5b5dcaf
Carlos Oliveira <carlosdtoliveira8@gmail.com>
2023-07-25 22:57:38 +0100

    adjusted main.go

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/blackout1208/go-parking
 
 go 1.20
-
-require gocv.io/x/gocv v0.33.0
--- main.go
+++ main.go
@@ -1,64 +1,26 @@
-// What it does:
-//
-// This example uses the VideoCapture class to capture AVI video from a connected webcam,
-// then saves 100 frames to a video file on disk.
-//
-// How to run:
-//
-// savevideo [camera ID] [video file]
-//
-// 		go run ./cmd/savevideo/main.go 0 testvideo.avi
-//
-
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"gocv.io/x/gocv"
+	"log"
+	"os/exec"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("How to run:\n\tsavevideo [camera ID] [video file]")
-		return
-	}
+	runCamera()
+}
 
-	deviceID := os.Args[1]
-	saveFile := os.Args[2]
-
-	webcam, err := gocv.OpenVideoCapture(deviceID)
+func runCamera() {
+	_, err := exec.Command("libcamera-vid", "-t", "10000", "-o", "test.h264", "--width", "1920", "--height", "1080").Output()
 	if err != nil {
-		fmt.Printf("Error opening video capture device: %v\n", deviceID)
-		return
-	}
-	defer webcam.Close()
-
-	img := gocv.NewMat()
-	defer img.Close()
-
-	if ok := webcam.Read(&img); !ok {
-		fmt.Printf("Cannot read device %v\n", deviceID)
-		return
+		log.Fatalln(err)
 	}
 
-	writer, err := gocv.VideoWriterFile(saveFile, "MJPG", 25, img.Cols(), img.Rows(), true)
+	log.Println("Video captured successfully")
+
+	_, err = exec.Command("touch", "./flag.txt").Output()
 	if err != nil {
-		fmt.Printf("error opening video writer device: %v\n", saveFile)
-		return
+		log.Fatalln(err)
 	}
-	defer writer.Close()
 
-	for i := 0; i < 100; i++ {
-		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("Device closed: %v\n", deviceID)
-			return
-		}
-		if img.Empty() {
-			continue
-		}
-
-		writer.Write(img)
-	}
+	log.Println("File touched successfully")
 }
