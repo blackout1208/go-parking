97da114dd110dbc21883e4a73e8e5761eb6c2496
Carlos Oliveira <carlosdtoliveira8@gmail.com>
2023-07-25 23:06:22 +0100

    handle os interruption

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
 )
 
 func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("captured %+v, exiting..", sig)
+			// sig is a ^C, handle it
+			touch()
+		}
+	}()
+
 	runCamera()
 }
 
@@ -14,10 +26,13 @@ func runCamera() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	log.Println("Video captured successfully")
 
-	_, err = exec.Command("touch", "./flag.txt").Output()
+	touch()
+}
+
+func touch() {
+	_, err := exec.Command("touch", "./flag.txt").Output()
 	if err != nil {
 		log.Fatalln(err)
 	}
