c4c4569d2541dbf0051534c7bcb859fe5ceba670
Carlos Oliveira <carlosdtoliveira8@gmail.com>
2023-07-26 01:24:42 +0100

    added sync wait groups

--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"sync"
 	"time"
 )
 
 const _videoCaptureTime = 5 * time.Second
 
 func main() {
-	messages := make(chan int)
-	var recording bool
+	var wg sync.WaitGroup
 
-	go func() { runCamera(&recording, messages) }()
-	go func() { processVideo(messages) }()
+	go func() { runCamera(&wg) }()
 
 	log.Println("Program started successfully..")
 
@@ -26,20 +25,10 @@ func main() {
 
 	go func() {
 		for sig := range c {
-			log.Printf("captured %+v, exiting..", sig)
-			// sig is a ^C, handle it
+			log.Println("captured ^C, exiting..", sig)
 
-			counter := 0
-			log.Println("Waiting for all images to be processed..", len(messages), recording)
-			for len(messages) > 0 || recording {
-				log.Println("Waiting for all images to be processed..")
-				time.Sleep(1 * time.Second)
-
-				if counter > 10 {
-					log.Println("Timeout reached, without all images being processed. Exiting...")
-					break
-				}
-			}
+			log.Println("Waiting for all images to be processed..")
+			wg.Wait()
 
 			kill <- true
 		}
@@ -47,36 +36,36 @@ func main() {
 	<-kill
 }
 
-func runCamera(recording *bool, messages chan int) {
+func runCamera(wg *sync.WaitGroup) {
 	for {
-		*recording = true
 		log.Println("Video capture started...")
 
-		_, err := exec.Command("libcamera-vid", "-t", "1000", "-o", "test.h264", "--width", "1920", "--height", "1080").Output()
+		_, err := exec.Command("libcamera-vid", "-t", fmt.Sprint(_videoCaptureTime), "-o", "test.h264", "--width", "1920", "--height", "1080").Output()
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		messages <- 1
-		*recording = false
 		log.Println("Video captured successfully")
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processVideo()
+		}()
 	}
 }
 
-func processVideo(messages chan int) {
-	for {
-		log.Println("Processing video...", len(messages))
+func processVideo() {
+	log.Println("Processing video...")
 
-		<-messages
+	now := time.Now()
+	// format now time to timestamp
+	timestamp := now.Format("2006-01-02_15-04-05")
 
-		now := time.Now()
-		// format now time to timestamp
-		timestamp := now.Format("2006-01-02_15-04-05")
-
-		_, err := exec.Command("ffmpeg", "-i", "test.h264", "-c:v", "copy", "-c:a", "copy", fmt.Sprint("html/", timestamp, ".mp4")).Output()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		log.Println("Processing video successfully completed")
+	_, err := exec.Command("ffmpeg", "-i", "test.h264", "-c:v", "copy", "-c:a", "copy", fmt.Sprint("html/", timestamp, ".mp4")).Output()
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	log.Println("Processing video successfully completed")
 }
